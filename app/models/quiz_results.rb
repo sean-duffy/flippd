4ae455569d6688b48f91b88c55a9b0e1697e9af0
GarethH9 <gareth@h9i.co.uk>
2016-02-15 17:29:26 +0000

    Increased field size for 'json_id' as some IDs in module.json were longer than the default limit. When this happened those records weren't being added to the database

--- app/models/quiz_results.rb
+++ app/models/quiz_results.rb
@@ -2,7 +2,7 @@ class QuizResult
   include DataMapper::Resource
 
   property :id, Serial
-  property :json_id, String, required: true
+  property :json_id, String, required: true, :length => 1000
   property :date, Date, required: true
   property :mark, Integer, required: true
 
