71600522d296a972e54d432685f15dcc968e0e16
slw546 <slw546@york.ac.uk>
2016-02-29 16:35:13 +0000

    Adds team routes - my team and member badge progression
    Adds routes and views to get team overview and badge progress of individuals in the team
    Displayed in the relevant view - team_view.erb or team_member_bades.erb
    
    Adds placeholder view and route for team badge progress (badges earnt as/by the team as a whole)

--- app/routes/teams.rb
+++ app/routes/teams.rb
@@ -3,10 +3,58 @@ require './app/helpers/badge_utils'
 require 'time'
 
 class Flippd < Sinatra::Application
-    helpers TeamUtils
+    helpers TeamUtils, BadgeUtils
     
     before do
         @teams = TeamUtils.load_teams(@module)
+    end
+
+    get '/teams/my_team' do
+        if @user_id == nil
+            redirect to("/")
+        end
+        @user = User.get(@user_id)
+        @team = TeamUtils.get_team_for_user(@user_id, @teams)
+        @members=[]
+        @team_badge_count = 0
+        @member_badges = 0
+        @team["members"].each do |member|
+            match = User.first(:email => member["email"])
+            badges = 0
+            if match != nil
+                badges = BadgeUtils.get_badge_count(match.id)
+            end
+            @member_badges = @member_badges + badges
+            @members.push(Hash["name"=>member["name"], "badges"=>badges])
+        end
+
+        erb :team_view
+    end
+
+    get '/teams/member_badges' do
+        if @user_id == nil
+            redirect to("/")
+        end
+        @user = User.get(@user_id)
+        @team = TeamUtils.get_team_for_user(@user_id, @teams)
+        @members=[]
+        @member_badges = 0
+        @badge_count = @badges.count
+        @team["members"].each do |member|
+            match = User.first(:email => member["email"])
+            badges = 0
+            if match != nil
+                badges = BadgeUtils.get_badge_count(match.id)
+            end
+            @member_badges = @member_badges + badges
+            badge_perc = badges.fdiv(@badge_count)*100
+            @members.push(Hash["name"=>member["name"], "badges"=>badges, "percent"=>badge_perc])
+        end
+        erb :team_member_badges
+    end
+
+    get '/teams/team_badges' do
+        redirect to("/teams/my_team")
     end
 
 end
